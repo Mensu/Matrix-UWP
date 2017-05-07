7c6944378374be3d55517598934fa58d17c4afef
陈序 <chenxu2048@gmail.com>
2017-05-07 17:22:49 +0800

    添加LoginUI

---
+++ Views/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Mntone.SvgForXaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace Matrix_UWP.Views {
+  /// <summary>
+  /// 可用于自身或导航至 Frame 内部的空白页。
+  /// </summary>
+  public sealed partial class Login : Page {
+    public Login() {
+      InitializeComponent();
+    }
+
+    private void LoginBtn_Click(object sender, RoutedEventArgs e) {
+    }
+
+    private void ResetPasswd_Click(object sender, RoutedEventArgs e) {
+
+    }
+  }
+}
