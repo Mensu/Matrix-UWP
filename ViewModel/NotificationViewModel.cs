0e6ac355d8402fd836d79d45fa03f814a752a129
Mensu <yxshw55@qq.com>
2017-05-08 21:17:11 +0800

    working on notification view

---
+++ ViewModel/NotificationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace Matrix_UWP.ViewModel {
+  class NotificationViewModel : BindableBase {
+    private ObservableCollection<Model.Notification> _list = new ObservableCollection<Model.Notification>() {
+      new Model.Notification(),
+      new Model.Notification()
+    };
+    public ObservableCollection<Model.Notification> list {
+      get {
+        return this._list;
+      }
+      set {
+        this.SetProperty(ref this._list, value);
+      }
+    }
+
+    public void updateWith(ObservableCollection<Model.Notification> newList) {
+      this.list = newList;
+    }
+  }
+}
---
+++ Views/NotificationList.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Matrix_UWP.Views {
+  public sealed partial class NotificationList : UserControl {
+    internal ViewModel.NotificationViewModel vm = new ViewModel.NotificationViewModel();
+    public NotificationList() {
+      this.InitializeComponent();
+    }
+
+    private async void listView_ItemClick(object sender, ItemClickEventArgs e) {
+      var notification = e.ClickedItem as Model.Notification;
+      await notification.toggleReadState();
+    }
+
+    public async Task refreshList() {
+      var newList = await Model.MatrixRequest.getNotificationList();
+      this.vm.updateWith(newList);
+    }
+
+    private async void checkbox_Checked(object sender, RoutedEventArgs e) {
+      //await this.refreshList();
+    }
+  }
+}
