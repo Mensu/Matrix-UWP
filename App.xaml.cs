d81210be6da40a61cc669da5bf146d8b0ae83bd1
Mensu <yxshw55@qq.com>
2017-05-09 21:51:17 +0800

    取消对App.xaml.cs的修改

--- App.xaml.cs
+++ App.xaml.cs
@@ -72,7 +72,7 @@ namespace Matrix_UWP
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Views.Login), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
