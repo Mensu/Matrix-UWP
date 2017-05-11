8f47f356cad29fdbeca79267dd0b514c65c7d6ed
陈序 <chenxu2048@gmail.com>
2017-05-11 14:30:33 +0800

    整合消息通知到主界面

---
+++ Converters/DoubleTo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Matrix_UWP.Converters {
+  class DoubleOffset : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, string language) {
+      return System.Convert.ToDouble(value ?? 0) + System.Convert.ToDouble(parameter ?? 0);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+      throw new NotImplementedException();
+    }
+  }
+}
+
