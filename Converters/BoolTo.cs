36cf61a015766163defdee75b0bda803e0d14587
陈序 <chenxu2048@gmail.com>
2017-05-11 09:49:52 +0800

    将PanOpenToMenuWidth转换器迁移到BoolToConverter中

--- Converters/BoolTo.cs
+++ Converters/BoolTo.cs
@@ -47,4 +47,13 @@ namespace Matrix_UWP.Converters {
       throw new NotImplementedException();
     }
   }
+  class BoolToHamburgerMenuWidth : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, string language) {
+      return (value ?? false).Equals(true) ? System.Convert.ToDouble(parameter ?? 0) : 48;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+      throw new NotImplementedException();
+    }
+  }
 }
