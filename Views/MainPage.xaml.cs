54d01ddc28290c6593e78fce41950bd65354790b
陈序 <chenxu2048@gmail.com>
2017-05-12 15:12:11 +0800

    将MainPage迁移到Views文件夹中

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -11,7 +11,7 @@ using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Matrix_UWP {
+namespace Matrix_UWP.Views {
   /// <summary>
   /// An empty page that can be used on its own or navigated to within a Frame.
   /// </summary>
