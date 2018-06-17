78570c92a348929b7e9943c601fd3d85decdd5b2
Mensu <yxshw55@qq.com>
2018-06-17 18:16:36 +0800

    添加NavigationView内容FramePage的共有接口

---
+++ Helpers/INavigationViewContent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_UWP.Helpers {
+  interface INavigationViewContent {
+    Task Refresh();
+  }
+}
